cfc9f5cbd491d6d311341ad24e9aa48e523f474d
Tarun Khurana <tarun.khurana@osttra.com>
2023-11-20 19:38:16 +0530

    prehooks testing

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Button, Input } from './components'
 //import Login from '@/pages/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
 
   return (
     <>
